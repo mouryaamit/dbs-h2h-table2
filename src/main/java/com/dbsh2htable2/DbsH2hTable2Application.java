50986dc2106a05456eb4b2f4bddf2a43d4578f8f
Amit Mourya <amourya@vsoftcorp.com>
2019-11-09 11:34:01 +0530

    initial commit

---
+++ src/main/java/com/dbsh2htable2/DbsH2hTable2Application.java
@@ -0,0 +1,13 @@
+package com.dbsh2htable2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbsH2hTable2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbsH2hTable2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbsh2htable2/DbsH2hTable2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbsh2htable2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbsH2hTable2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
